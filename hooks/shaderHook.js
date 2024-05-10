003ad2d98c34cc83a76b6f68a06bca1faecf170b
Cha-er-si <vpcharles31@gmail.com>
2024-05-10 11:02:29 +0800

    Hook fix

--- hooks/shaderHook.js
+++ hooks/shaderHook.js
@@ -13,25 +13,48 @@ module.exports = function (context) {
     }
 
     const pluginsBlock = `
-        plugins {
-            id 'com.github.johnrengelman.shadow' version '8.1.1'
-            id 'java'
-        }`;
+            plugins {
+                id 'com.github.johnrengelman.shadow' version '8.1.1'
+                id 'java'
+            }`;
 
-    if (data.includes("plugins {")) {
-      // Append to existing plugins block
-      const result = data.replace(
-        /plugins\s*{/,
-        `$&\n    id 'com.github.johnrengelman.shadow' version '8.1.1'\n    id 'java'`
-      );
+    // Determine the location to insert the plugins block
+    const buildScriptEndIndex =
+      data.indexOf("}", data.indexOf("buildscript")) + 1;
+    if (buildScriptEndIndex > -1 && data.indexOf("plugins {") === -1) {
+      // Insert plugins block after buildscript block
+      const result =
+        data.slice(0, buildScriptEndIndex) +
+        pluginsBlock +
+        data.slice(buildScriptEndIndex);
       fs.writeFile(gradleBuildFilePath, result, "utf8", function (err) {
         if (err) return console.log(err);
+        console.log("Successfully added plugins block after buildscript.");
       });
+    } else if (data.indexOf("plugins {") > -1) {
+      // Plugins block already exists, append if necessary
+      if (
+        !data.includes("id 'com.github.johnrengelman.shadow' version '8.1.1'")
+      ) {
+        const modifiedPluginsBlock = data.replace(
+          /plugins\s*{/,
+          `$&\n    id 'com.github.johnrengelman.shadow' version '8.1.1'`
+        );
+        const finalData = modifiedPluginsBlock.replace(
+          /plugins\s*{/,
+          `$&\n    id 'java'`
+        );
+        fs.writeFile(gradleBuildFilePath, finalData, "utf8", function (err) {
+          if (err) return console.log(err);
+          console.log("Successfully appended to existing plugins block.");
+        });
+      }
     } else {
-      // Insert plugins block at the beginning of the file
+      // No buildscript block or plugins block, prepend to the file
       const result = pluginsBlock + "\n" + data;
       fs.writeFile(gradleBuildFilePath, result, "utf8", function (err) {
         if (err) return console.log(err);
+        console.log("Plugins block added at the beginning of the file.");
       });
     }
   });

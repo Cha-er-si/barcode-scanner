8e775af3838273038c064b952bd65c3f9964776f
Cha-er-si <vpcharles31@gmail.com>
2024-05-10 10:56:34 +0800

    Hook fix

--- hooks/shaderHook.js
+++ hooks/shaderHook.js
@@ -2,12 +2,9 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = function (context) {
-  const root = context.opts.projectRoot;
   const gradleBuildFilePath = path.join(
-    root,
-    "platforms",
-    "android",
-    "build.gradle"
+    context.opts.projectRoot,
+    "platforms/android/app/build.gradle"
   );
 
   fs.readFile(gradleBuildFilePath, "utf8", function (err, data) {
@@ -15,15 +12,24 @@ module.exports = function (context) {
       throw new Error("Unable to find Android build.gradle: " + err);
     }
 
-    if (!data.includes("id 'com.github.johnrengelman.shadow'")) {
-      const result = data.replace(
-        /plugins \{/,
-        `plugins {
-                id 'com.github.johnrengelman.shadow' version '8.1.1'
-                id 'java'
-            `
-      );
+    const pluginsBlock = `
+        plugins {
+            id 'com.github.johnrengelman.shadow' version '8.1.1'
+            id 'java'
+        }`;
 
+    if (data.includes("plugins {")) {
+      // Append to existing plugins block
+      const result = data.replace(
+        /plugins\s*{/,
+        `$&\n    id 'com.github.johnrengelman.shadow' version '8.1.1'\n    id 'java'`
+      );
+      fs.writeFile(gradleBuildFilePath, result, "utf8", function (err) {
+        if (err) return console.log(err);
+      });
+    } else {
+      // Insert plugins block at the beginning of the file
+      const result = pluginsBlock + "\n" + data;
       fs.writeFile(gradleBuildFilePath, result, "utf8", function (err) {
         if (err) return console.log(err);
       });

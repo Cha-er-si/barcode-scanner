c9e8ad7f221795f3084f205d8fa7df6fe7bab8f8
Cha-er-si <vpcharles31@gmail.com>
2024-05-13 10:38:15 +0800

    Hooks Fix

--- hooks/shaderHook.js
+++ hooks/shaderHook.js
@@ -1,59 +1,33 @@
-var fs = require("fs");
-var path = require("path");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = function (context) {
-  var rootDir = context.opts.projectRoot;
-  var gradleBuildFile = path.join(
-    rootDir,
-    "platforms",
-    "android",
-    "app",
-    "build.gradle"
+  const gradleBuildFile = path.join(
+    context.opts.projectRoot,
+    "platforms/android/build.gradle"
   );
+  let buildGradle = fs.readFileSync(gradleBuildFile, "utf8");
 
-  console.log("Attempting to read build.gradle from: ", gradleBuildFile);
+  // Check if the plugin is already added
+  if (!buildGradle.includes("com.github.johnrengelman.shadow")) {
+    // Define the plugin block
+    const pluginToAdd = `plugins {
+            id "com.github.johnrengelman.shadow" version "7.1.1"
+        }
+        `;
 
-  fs.readFile(gradleBuildFile, "utf8", function (err, data) {
-    if (err) {
-      console.log("Error reading build.gradle: ", err);
-      return;
-    }
-
-    var modified = false;
-
-    if (!data.includes("com.github.johnrengelman.shadow")) {
-      var pluginToAdd =
-        "id 'com.github.johnrengelman.shadow' version '7.1.1'\n";
-      data = data.replace("plugins {", `plugins {\n    ${pluginToAdd}`);
-      modified = true;
-    }
-
-    if (!data.includes("shadowJar {")) {
-      var shadowConfig = `
+    /*
+        
 shadowJar {
     relocate 'com.google.zxing', 'shadowed.com.google.zxing'
     relocate 'com.journeyapps', 'shadowed.com.journeyapps'
 }
 
 tasks.build.dependsOn shadowJar
-`;
-      var position = data.lastIndexOf("}");
-      data =
-        data.substring(0, position) + shadowConfig + data.substring(position);
-      modified = true;
-    }
+ */
 
-    if (modified) {
-      console.log("Writing modifications to build.gradle...");
-      fs.writeFile(gradleBuildFile, data, "utf8", function (writeErr) {
-        if (writeErr) {
-          console.log("Error writing build.gradle: ", writeErr);
-        } else {
-          console.log("build.gradle updated successfully.");
-        }
-      });
-    } else {
-      console.log("No modifications needed for build.gradle.");
-    }
-  });
+    // Insert the plugin block at the top of the file
+    buildGradle = pluginToAdd + buildGradle;
+    fs.writeFileSync(gradleBuildFile, buildGradle, "utf8");
+  }
 };

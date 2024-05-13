40b66d757bc378cff37c1376342b0336ff868600
Cha-er-si <vpcharles31@gmail.com>
2024-05-13 16:08:53 +0800

    Gradle and Hooks Fix

--- hooks/shaderHook.js
+++ hooks/shaderHook.js
@@ -8,12 +8,17 @@ module.exports = function (context) {
   );
   let buildGradle = fs.readFileSync(gradleBuildFile, "utf8");
 
+  const libFolder = path.join(
+    context.opts.projectRoot,
+    "platforms/android/src/main/libs"
+  );
+
   // Define the plugin block
   const pluginToAdd = `plugins {
     id "com.github.johnrengelman.shadow" version "7.1.2"
     id "java"
   }
-  
+
   import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
   
   task customShadowJar(type: ShadowJar) {
@@ -24,7 +29,7 @@ module.exports = function (context) {
       archiveVersion.set('1.0.0')
       archiveClassifier.set('')
   
-      destinationDirectory.set(file("${project.rootDir}/android/src/main/libs"))
+      destinationDirectory.set(file("${libFolder}"))
   }
   
   build.dependsOn customShadowJar

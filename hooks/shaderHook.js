0c80ba21cc7a6744882e053d1bd9f02ae1874834
Cha-er-si <vpcharles31@gmail.com>
2024-05-13 15:08:24 +0800

    Gradle Fix

--- hooks/shaderHook.js
+++ hooks/shaderHook.js
@@ -11,6 +11,7 @@ module.exports = function (context) {
   // Define the plugin block
   const pluginToAdd = `plugins {
     id "com.github.johnrengelman.shadow" version "7.1.2"
+    id "java"
   }
     `;
 

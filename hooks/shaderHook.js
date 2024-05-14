323851a955af288417eb99953b2d1a919720b186
Cha-er-si <vpcharles31@gmail.com>
2024-05-14 09:58:05 +0800

    Hooks Update

--- hooks/shaderHook.js
+++ hooks/shaderHook.js
@@ -8,25 +8,45 @@ module.exports = function (context) {
   );
   let buildGradle = fs.readFileSync(gradleBuildFile, "utf8");
 
+  const classPath = `classpath "gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.0.0"`;
+
+  const dependencyRegex = /(dependencies \{[^}]*)(\})/;
+
+  if (dependencyRegex.test(buildGradle)) {
+    buildGradle = buildGradle.replace(
+      dependencyRegex,
+      `$1\n${classPath}\n    $2`
+    );
+  }
+
   // Define the plugin block
   const pluginToAdd = `plugins {
-    id "com.github.johnrengelman.shadow" version "7.1.1"
     id "java"
+  }
+  
+  apply plugin: "com.github.johnrengelman.shadow"
+  
+  import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+  
+  task customShadowJar(type: ShadowJar) {
+    relocate 'com.google.zxing', 'chaersi.shaded.zxing'
+    relocate 'com.journeyapps', 'chaersi.shaded.journeyapps'
+  
+    archiveBaseName.set('chaersi-shaded-library')
+    archiveVersion.set('1.0.0')
+    archiveClassifier.set('')
+  
+    destinationDirectory.set(file("/app/src/main/libs"))
   }`;
 
-  // const shaderConfig = `
-  //   `;
-
-  // buildGradle = pluginToAdd + buildGradle;
-
-  // if (buildGradle.includes("allprojects {")) {
-  //   buildGradle = buildGradle.replace(
-  //     /allprojects \{/g,
-  //     shaderConfig + "allprojects {"
-  //   );
-  // } else {
-  //   buildGradle = shaderConfig + buildGradle;
-  // }
+  if (buildGradle.includes("allprojects {")) {
+    buildGradle = buildGradle.replace(
+      /allprojects \{/g,
+      pluginToAdd + "allprojects {"
+    );
+  } else {
+    buildGradle = pluginToAdd + buildGradle;
+  }
 
   fs.writeFileSync(gradleBuildFile, buildGradle, "utf8");
 };

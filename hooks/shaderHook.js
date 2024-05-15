27f43fdcc7b92f1c0f6febb20371e40fdc644fe0
Cha-er-si <vpcharles31@gmail.com>
2024-05-15 09:18:07 +0800

    Hooks

--- hooks/shaderHook.js
+++ hooks/shaderHook.js
@@ -8,41 +8,41 @@ module.exports = function (context) {
   );
   let buildGradle = fs.readFileSync(gradleBuildFile, "utf8");
 
-  const classPath = `classpath "gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.0.0"\n`;
+  // const classPath = `classpath "gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.0.0"\n`;
 
-  const dependencyRegex = /(dependencies \{[^}]*)(\})/;
+  // const dependencyRegex = /(dependencies \{[^}]*)(\})/;
 
-  if (dependencyRegex.test(buildGradle)) {
-    buildGradle = buildGradle.replace(
-      /(\s*\/\/ in the individual module build.gradle files[^\}]*)(\s*\})/,
-      `$1\n${classPath}\n$2`
-    );
-  }
+  // if (dependencyRegex.test(buildGradle)) {
+  //   buildGradle = buildGradle.replace(
+  //     /(\s*\/\/ in the individual module build.gradle files[^\}]*)(\s*\})/,
+  //     `$1\n${classPath}\n$2`
+  //   );
+  // }
 
-  const destionationPath = path.join(
-    context.opts.projectRoot,
-    "platforms/android/app/src/main/libs"
-  );
+  // const destionationPath = path.join(
+  //   context.opts.projectRoot,
+  //   "platforms/android/app/src/main/libs"
+  // );
 
   // Define the plugin block
   const pluginToAdd = `plugins {
     id "java"
-  }
-  
-  apply plugin: "com.github.johnrengelman.shadow"
-  
-  import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-  
-  task customShadowJar(type: ShadowJar) {
-    relocate 'com.google.zxing', 'chaersi.shaded.zxing'
-    relocate 'com.journeyapps', 'chaersi.shaded.journeyapps'
-  
-    // archiveBaseName.set('chaersi-shaded-library')
-    // archiveVersion.set('1.0.0')
-    // archiveClassifier.set('')
-  }
-  
-  compileJava.dependsOn customShadowJar`;
+  }`;
+
+  // `apply plugin: "com.github.johnrengelman.shadow"
+
+  // import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+  // task customShadowJar(type: ShadowJar) {
+  //   relocate 'com.google.zxing', 'chaersi.shaded.zxing'
+  //   relocate 'com.journeyapps', 'chaersi.shaded.journeyapps'
+
+  //   // archiveBaseName.set('chaersi-shaded-library')
+  //   // archiveVersion.set('1.0.0')
+  //   // archiveClassifier.set('')
+  // }
+
+  // compileJava.dependsOn customShadowJar`;
 
   //   destinationDirectory.set(file('${destionationPath
   //     .toString()
